9f64039351795c99250741885df819a0d50d89ba
Svend Aage Roperos Nielsen <shaddyd-sarn@live.dk>
2020-02-28 14:50:34 +0100

    Cleanup

--- CookIt.API/Models/Ingredient.cs
+++ CookIt.API/Models/Ingredient.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using static UdpipeLibrary.UdpipeWrapper;
 
 namespace CookIt.API.Models
 {
---
+++ TestLibraries/Program.cs
@@ -0,0 +1,17 @@
+﻿using CstLemmaLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace TestLibraries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string>() { new string("hello") };
+            Dictionary<string, List<string>> lemmasByIngredientName = CstLemmaWrapper.GetLemmasByTextDictionary(strings);
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
